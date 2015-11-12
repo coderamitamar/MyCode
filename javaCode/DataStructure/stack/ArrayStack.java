b263ebcde6417295523f333f858e7d0b1f926993
coderamitamar <coderamitamar@users.noreply.github.com>
2015-11-12 23:40:27 +0530

    Update ArrayStack.java

--- javaCode/DataStructure/stack/ArrayStack.java
+++ javaCode/DataStructure/stack/ArrayStack.java
@@ -1,68 +1,54 @@
 package stack;
-public class ArrayStack
-{
+public class ArrayStack{
 	//Variable section
 	private Object stk[];
 	private int top;
 	private int maxSize;
 	
 	//Constructor section
-	public ArrayStack()
-	{
+	public ArrayStack(){
 		maxSize=1;
 		stk=new Object[1];
 		top=-1;
 	}
-	public ArrayStack(int size)
-	{
+	public ArrayStack(int size){
 		maxSize=size;
 		stk=new Object[size];
 		top=-1;
 	}
 	
 	//Method section
-	public int size()
-	{
+	public int size(){
 		return top+1;
 	}
-	public boolean isEmpty()
-	{
+	public boolean isEmpty(){
 		return (top<0);
 	}
-	public void push(Object o)
-	{
+	public void push(Object o){
 		int i=0;
-		if(this.size()==maxSize)
-		{
+		if(this.size()==maxSize){
 			Object o1[]=new Object[2*maxSize];
 			for(Object k:stk)
 				o1[i++]=k;
 			stk=o1;
 			stk[++top]=o;
-		}
-		else
+		}else
 			stk[++top]=o;
 	}
-	public Object pop()
-	{
+	public Object pop(){
 		Object o;
-		try
-		{
+		try{
 			o=stk[top];
 			stk[top--]=null;
 			return o;
-		}
-		catch(ArrayIndexOutOfBoundsException e)
-		{
+		}catch(ArrayIndexOutOfBoundsException e){
 			System.out.println("Stack underflow!!!");
 			return "NotOk";
 		}
 	}
-	public void show()
-	{
+	public void show(){
 		int i;
-		if(top<0)
-		{
+		if(top<0){
 			System.out.println("Stack is empty!!!\n");
 			return;
 		}
