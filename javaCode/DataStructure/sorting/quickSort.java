ac1d2f0cd7aad961ea87d65c37afc13c6e89a282
coderamitamar <coderamitamar@users.noreply.github.com>
2015-11-12 23:48:11 +0530

    Update quickSort.java

--- javaCode/DataStructure/sorting/quickSort.java
+++ javaCode/DataStructure/sorting/quickSort.java
@@ -1,27 +1,23 @@
 package sorting;
-public class quickSort
-{
-	public int partition(int A[],int beg,int end)
-	{
+public class quickSort{
+	public int partition(int A[],int beg,int end){
 		int loc=beg,i=beg,j=end,temp;
 		boolean flag=false;
-		while(flag!=true)
-		{
+		while(flag!=true){
 			while(A[loc]<=A[j] && loc!=j)	j--;
-			if(loc==j)	flag=true;
-			else
-			{
+			if(loc==j)
+				flag=true;
+			else{
 				temp=A[loc];
 				A[loc]=A[j];
 				A[j]=temp;
 				loc=j;
 			}
-			if(flag!=true)
-			{
+			if(flag!=true){
 				while(A[loc]>=A[i] && loc!=i)	i++;
-				if(loc==i)	flag=true;
-				else
-				{
+				if(loc==i)
+					flag=true;
+				else{
 					temp=A[loc];
 					A[loc]=A[i];
 					A[i]=temp;
@@ -31,11 +27,9 @@ public class quickSort
 		}
 		return loc;
 	}
-	public void qckSort(int A[],int p,int r)
-	{
+	public void qckSort(int A[],int p,int r){
 		int pivot;
-		if(p<r)
-		{
+		if(p<r){
 			pivot=partition(A,p,r);
 			qckSort(A,p,pivot-1);
 			qckSort(A,pivot+1,r);
